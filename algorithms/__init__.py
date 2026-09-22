5ed84ff11a82fdcf3f7df4a73f839d97cb21c47e
Hassan saleemi <saleemihassan597@gmail.com>
2026-09-22 07:49:41 +0500

    add algorithms package initialization

--- algorithms/__init__.py
+++ algorithms/__init__.py
@@ -1,17 +1,1 @@
-from .fedavg import FedAvgTrainer
-from .fedprox import FedProxTrainer
-
-TRAINER_REGISTRY = {
-    "fedavg": FedAvgTrainer,
-    "fedprox": FedProxTrainer,
-}
-
-
-def get_trainer(algorithm: str, **kwargs):
-    try:
-        trainer_cls = TRAINER_REGISTRY[algorithm]
-    except KeyError:
-        raise ValueError(
-            f"Unknown algorithm '{algorithm}'. Available: {list(TRAINER_REGISTRY)}"
-        )
-    return trainer_cls(**kwargs)
+"""Federated learning strategy definitions."""
